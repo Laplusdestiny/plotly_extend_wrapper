5cb03b19fcefb19f9cfa81068462ba4e3874b1d4
Toru Sumi <prayonshootingstars@gmail.com>
2024-01-28 16:28:32 +0900

    Started to test source code and using codecov - Added unit test code - Made github actions configuration file

--- test/test_wrapper.py
+++ test/test_wrapper.py
@@ -1,17 +1,20 @@
 # encoding: utf-8
 
-import pandas as pd
 from sklearn.datasets import load_iris, load_diabetes
-from plotly_extend_wrapper.wrapper import *
+from plotly_extend_wrapper.wrapper import Plot_pie, Plot_sunburst
+from plotly import graph_objects as go
+
 
 def test_pie():
     data = load_iris(as_frame=True)
 
     plot = Plot_pie(data["frame"], "target")
+    assert isinstance(plot, go.Figure)
+
 
 def test_sunburst():
     data = load_diabetes(as_frame=True)["frame"]
     data["sex"] = data["sex"] >= 0
 
     plot = Plot_sunburst(data, ["target", "sex"])
-
+    assert isinstance(plot, go.Figure)
