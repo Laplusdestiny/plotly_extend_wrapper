18542b75e586dd67691daa0a991753604b08ac65
Laplusdestiny <prayonshootingstars@gmail.com>
2024-11-16 09:06:10 +0000

    check_directory関数を改善し、ディレクトリが存在しない場合に作成する機能を追加

--- plotly_extend_wrapper/common.py
+++ plotly_extend_wrapper/common.py
@@ -1,12 +1,21 @@
 from pathlib import Path
 
 
-def check_directory(file_p):
-    file_p = Path(file_p)
-    if file_p.suffix == "":
-        dir_p = file_p
+def check_directory(file_path):
+    """
+    Ensures that the directory for the given file path exists, creating it if necessary.
+
+    Args:
+        file_path (str or Path): The file path for which to check the directory.
+
+    Returns:
+        Path: The resolved absolute path of the given file path.
+    """
+    file_path = Path(file_path)
+    if not file_path.suffix:
+        dir_p = file_path
     else:
-        dir_p = file_p.parent
+        dir_p = file_path.parent
     if not dir_p.is_dir():
-        dir_p.mkdir(parents=True)
-    return file_p.resolve()
+        dir_p.mkdir(parents=True, exist_ok=True)
+    return file_path.resolve()
